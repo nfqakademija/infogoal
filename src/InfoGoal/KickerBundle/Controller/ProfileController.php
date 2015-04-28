60fd2860f683f1c832ac1aeb30972b164f20be1c
Albertas <albertas47@gmail.com>
2015-04-28 18:34:27 +0300

    Updated to view database data

--- src/InfoGoal/KickerBundle/Controller/ProfileController.php
+++ src/InfoGoal/KickerBundle/Controller/ProfileController.php
@@ -2,14 +2,32 @@
 
 namespace InfoGoal\KickerBundle\Controller;
 
+use InfoGoal\KickerBundle\Service\ExpCalculator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProfileController extends Controller
 {
     public function indexAction()
     {
+        $em =  $em = $this->getDoctrine()->getManager();
+        $calculate = new ExpCalculator($em);
+
+        $calculate->Calculate(1,10,True,10);
+
+        $player = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Player')->find(1);
+
         return $this->render('InfoGoalKickerBundle:Profile:index.html.twig', array(
-                // ...
-            ));    }
+            'player' => $player
+        ));
+//        $player = new Player();
+//        $player->setId(1);
+//        $player->setCardId(0004255210);
+//        $player->setName("Albertas Balzarevicius");
+//          $player->setXp(0);
+//          $player->
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($player);
+//        $em->flush();
+    }
 
 }
