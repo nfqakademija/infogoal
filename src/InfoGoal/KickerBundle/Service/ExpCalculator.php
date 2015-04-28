0995828b707a4358c665b436555af17837d7d15d
Albertas <albertas47@gmail.com>
2015-04-28 18:36:26 +0300

    ExpCalculator update

--- src/InfoGoal/KickerBundle/Service/ExpCalculator.php
+++ src/InfoGoal/KickerBundle/Service/ExpCalculator.php
@@ -3,18 +3,34 @@ namespace InfoGoal\KickerBundle\Service;
 
 use InfoGoal\KickerBundle\Player;
 
+
 class ExpCalculator {
-    public function Calculate($time, $win, $goals ){
+    private $em;
+
+    function __construct($em)
+    {
+        $this->em = $em;
+    }
+
+    public function Calculate($id, $time, $win, $goals ){
         $exp = $time*5 + $goals*10 + 50;
 
         if ($win === true)
             $this->$exp = + 50;
 
-        $player = new Player();
+//        $this->em = $this->getDoctrine()->getManager();
 
-        //...
+        $player = $this->em->getRepository('InfoGoalKickerBundle:Player')->find($id);
 
+        $player->setXp($player->getXp() + $exp);
 
+        if ($player->getXp() >= $player->getLevelXp() )
+        {
+            $player->setLevel($player->getLevel() + 1);
+            $player->setLevelXp($player->getLevelXp()*1.5);
+        }
+        $this->em->persist($player);
+        $this->em->flush();
 
     }
 } 
