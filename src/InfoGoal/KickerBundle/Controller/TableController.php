5df3e15c005696480c7d162baace663aaf6c4117
Aurelija <esuaurelija@gmail.com>
2015-05-04 22:19:48 +0300

    displaying table info from database

--- src/InfoGoal/KickerBundle/Controller/TableController.php
+++ src/InfoGoal/KickerBundle/Controller/TableController.php
@@ -8,11 +8,17 @@ class TableController extends Controller
 {
         public function indexAction()
     {
-        $status = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:TableOption')->findOneBy(array("optionKey" => "table_status"));
-        if($status){
+        $state = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:TableOption')->findOneBy(array("optionKey" => "table_state"));
+        $stateValue = $state->getOptionValue();
+        if($stateValue == 0){
             return $this->render('InfoGoalKickerBundle:Table:index.html.twig', array( ));
+        } elseif($stateValue == 1) {
+            $gameId = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:TableOption')->findOneBy(array("optionKey" => "active_game_id"));
+            $game = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Game')->findOneBy(array("id" => $gameId->getOptionValue()));
+
+            return $this->render('InfoGoalKickerBundle:Table:game.html.twig', array('game' => $game));
         } else {
-            return $this->render('InfoGoalKickerBundle:Table:game.html.twig', array( ));
+            return $this->render('@InfoGoalKicker/Table/reservation.html.twig');
         }
 
     }
