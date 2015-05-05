c451439c311d21ee9bd2c753c4f0aae8d9ed4858
Albertas <albertas47@gmail.com>
2015-05-05 17:45:39 +0300

    Deleted hardcode

--- src/InfoGoal/KickerBundle/Controller/ProfileController.php
+++ src/InfoGoal/KickerBundle/Controller/ProfileController.php
@@ -8,25 +8,10 @@ class ProfileController extends Controller
 {
     public function indexAction()
     {
-        $em =  $em = $this->getDoctrine()->getManager();
-        $calculate = new ExpCalculator($em);
 
-        $calculate->Calculate(1,10,True,10);
-
-        $player = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Player')->find(1);
 
         return $this->render('InfoGoalKickerBundle:Profile:index.html.twig', array(
-            'player' => $player
         ));
-//        $player = new Player();
-//        $player->setId(1);
-//        $player->setCardId(0004255210);
-//        $player->setName("Albertas Balzarevicius");
-//          $player->setXp(0);
-//          $player->
-//        $em = $this->getDoctrine()->getManager();
-//        $em->persist($player);
-//        $em->flush();
     }
 
 }
