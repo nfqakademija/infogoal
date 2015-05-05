a6ba9e4290e61e12073a511785e84b24fc877a98
Aurelija Kunceviciute <esu.aurelija@gmail.com>
2015-05-05 18:43:13 +0300

    added variables to templates

--- src/InfoGoal/KickerBundle/Controller/ProfileController.php
+++ src/InfoGoal/KickerBundle/Controller/ProfileController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProfileController extends Controller
 {
-    public function indexAction()
+    public function indexAction($id)
     {
 
+       $player = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Player')->findOneBy(array("id" => $id));
 
         return $this->render('InfoGoalKickerBundle:Profile:index.html.twig', array(
+            'player' => $player
         ));
     }
 
--- src/InfoGoal/KickerBundle/Controller/TimelineController.php
+++ src/InfoGoal/KickerBundle/Controller/TimelineController.php
@@ -15,9 +15,9 @@ class TimelineController extends Controller {
 
     public function indexAction()
     {
-        return $this->render('InfoGoalKickerBundle:Timeline:index.html.twig', array(
-
-        ));
+        $gameId = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:TableOption')->findOneBy(array("optionKey" => "active_game_id"));
+        $game = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Game')->findOneBy(array("id" => $gameId->getOptionValue()));
+        return $this->render('InfoGoalKickerBundle:Timeline:index.html.twig', array( 'game' => $game ));
     }
 
 }
