a97b47269baabcd8e919db61e0aae6579f2ddecc
Albertas <albertas47@gmail.com>
2015-05-08 23:14:58 +0300

    Created function getBadge

--- src/InfoGoal/ApiBundle/Service/Achievements.php
+++ src/InfoGoal/ApiBundle/Service/Achievements.php
@@ -11,6 +11,9 @@ use DateTime;
 
 class Achievements
 {
+    /**
+     * @var EntityManager
+     */
     private $em;
 
     function __construct($em)
@@ -19,44 +22,48 @@ class Achievements
     }
 
 
-    public function checkBadges($player)
+    public function checkBadges($game)
     {
+        $players = array();
+        array_push($players, $game->getPlayer1(), $game->getPlayer2(),
+            $game->getPlayer3(), $game->getPlayer4());
+
         $badges = $this->em->getRepository('InfoGoalKickerBundle:Badge')->findAll();
+        foreach ($players as $player) {
+            if ($player != null) {
+                $playerInfo = array();
 
-        $playerInfo = array();
+                $playerInfo['win'] = $player->getWon();
 
-//        var_dump($badges);
+                $playerInfo['played'] = $player->getPlayed();
 
-        $playerInfo['win'] = $player->getWon();
-
-        $playerInfo['played'] = $player->getPlayed();
-
-        foreach ($badges as $badge) {
-            $rule = explode('::', $badge->getRulesToGetBadge());
-//isset($playerInfo[$rule[0]]) &&
-            if ($playerInfo[$rule[0]] >= $rule[1]) {
-
-                echo $rule[0]."<br>";
-                echo $playerInfo[$rule[0]]." ".$rule[1]."<br>";
-
-                $playerBadge = new PlayersBadges();
-
-                echo $player->getId()." ".$badge->getId()."<br>";
-
-                $playerBadge->setPlayerId($player->getId());
-                $playerBadge->setBadgeId($badge->getId());
-
-                $date = new DateTime();
-                $date->getTimestamp();
-                $playerBadge->setDate($date);
-
-                echo $playerBadge->getPlayerId()." ";
-                echo $playerBadge->getBadgeId()."<br>";
-
-                $this->em->persist($playerBadge);
-                $this->em->flush();
+                foreach ($badges as $badge) {
+                    $rule = explode('::', $badge->getRulesToGetBadge());
+                    if ($playerInfo[$rule[0]] >= $rule[1]) {
+                        $this->getBadge($player, $badge);
+                    }
+                }
             }
         }
+    }
 
+    private function getBadge($player, $badge)
+    {
+        $playerBadgeDup = $this->em->getRepository('InfoGoalKickerBundle:PlayersBadges')->findOneBy(
+            array('players' => $player, 'badges' => $badge)
+        );
+        if (!isset($playerBadgeDup)) {
+            $playerBadge = new PlayersBadges();
+
+            $playerBadge->setPlayers($player);
+            $playerBadge->setBadges($badge);
+
+            $date = new DateTime();
+            $date->getTimestamp();
+            $playerBadge->setDate($date);
+
+            $this->em->persist($playerBadge);
+            $this->em->flush();
+        }
     }
 } 
