3c5e7152db649dd7f6c52a01a5570a796c9f406b
Aurelija Kunceviciute <esu.aurelija@gmail.com>
2015-05-08 22:52:33 +0300

    added badges to user profile

--- src/InfoGoal/KickerBundle/Controller/ProfileController.php
+++ src/InfoGoal/KickerBundle/Controller/ProfileController.php
@@ -9,10 +9,14 @@ class ProfileController extends Controller
     public function indexAction($id)
     {
 
-       $player = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Player')->findOneBy(array("id" => $id));
-
+        $player = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Player')->findOneBy(array("id" => $id));
+        $badgesId = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:PlayersBadges')->findBy(array("playerId" => $id));
+        $badges = $this->getDoctrine()->getRepository('InfoGoalKickerBundle:Badge')->findBy(array("id" => $badgesId));
+        $badgesCount = count($badges);
         return $this->render('InfoGoalKickerBundle:Profile:index.html.twig', array(
-            'player' => $player
+            'player' => $player,
+            'badges' => $badges,
+            'badgesCount' => $badgesCount
         ));
     }
 
