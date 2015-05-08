367816f8e3a8e84faa81665c30cb4499d3a7545c
Albertas <albertas47@gmail.com>
2015-05-08 12:30:02 +0300

    Hardcoded for testing

--- src/InfoGoal/ApiBundle/Controller/ApiController.php
+++ src/InfoGoal/ApiBundle/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace InfoGoal\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use InfoGoal\ApiBundle\Service\Api;
+use InfoGoal\ApiBundle\Service\Achievements;
 
 
 class ApiController extends Controller
@@ -11,9 +12,19 @@ class ApiController extends Controller
 
     public function indexAction()
     {
-        $api = $this->get('kicker.api');
+//        $api = $this->get('kicker.api');
         $em = $this->getDoctrine()->getManager();
-        $options = $em->getRepository('InfoGoalKickerBundle:TableOption')->findAll();
-        return $api->readApi($options);
+//        $options = $em->getRepository('InfoGoalKickerBundle:TableOption')->findAll();
+//        return $api->readApi($options);
+        $badge = new Achievements($em);
+
+        $player = $em->getRepository('InfoGoalKickerBundle:Player')->find(7);
+
+        $badge->checkBadges($player);
+
+        return $this->render('InfoGoalKickerBundle:Profile:index.html.twig', array(
+            'player' => $player
+        ));
+
     }
 } 
