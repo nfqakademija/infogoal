5af962bf071cef0c48fd0809bf5c84d1ecd948c5
ernis9112 <ernis9112@gmail.com>
2015-05-11 20:06:53 +0300

    Table reservation front end side.

---
+++ src/InfoGoal/KickerBundle/Resources/public/js/reservation.js
@@ -0,0 +1,26 @@
+jQuery("#makeReservation").click(function () {
+
+    var reservationDialog = jQuery("#reservationDialog");
+    var backOverlay = jQuery("#backgroundOverlay");
+
+    backOverlay.removeClass("hidden");
+    reservationDialog.removeClass("hidden");
+
+    reservationDialog.dialog({
+        resizable: false,
+        draggable: false,
+        width: 350,
+        height: 150,
+        modal: false,
+        buttons: {
+            "Patvirtinti": function () {
+                jQuery(this).dialog("close");
+                backOverlay.addClass("hidden");
+            },
+            "Atšaukti": function () {
+                jQuery(this).dialog("close");
+                backOverlay.addClass("hidden");
+            }
+        }
+    });
+});
