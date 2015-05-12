c0c8615ab1d5c5da4ed3245b37e60e642d2576a3
ernis9112 <ernis9112@gmail.com>
2015-05-12 12:43:42 +0300

    API update command init.

---
+++ src/InfoGoal/ApiBundle/Command/ApiUpdateCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace InfoGoal\ApiBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ApiUpdateCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('api:update')
+            ->setDescription('Updates data from API.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $api = $this->getContainer()->get('kicker.api');
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $optionsRepo = $em->getRepository('InfoGoalKickerBundle:TableOption');
+        //$options = $optionsRepo->findAll();
+
+        //while (true) {
+            //$api->readApi($options);
+            //sleep(2);
+        //}
+
+        $output->writeln("Data updated.");
+    }
+}
